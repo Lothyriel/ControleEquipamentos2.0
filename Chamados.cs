0aa0d4ac6cdea1ff5d1b973f3df1d47bd353c139
Lothyriel <56160019+Lothyriel@users.noreply.github.com>
2021-04-16 08:37:47 -0300

    Initial commit

---
+++ Chamados.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Equipamentos_Junior
+{
+    class Chamados
+    {
+        private string titulo;
+        private string desc;
+        private Equipamentos equipamento;
+        private DateTime data_abertura;
+
+        public Chamados(string titulo, String desc, Equipamentos equipamento, DateTime data_abertura)
+        {
+            this.titulo = titulo;
+            this.desc = desc;
+            this.equipamento = equipamento;
+            this.data_abertura = data_abertura;
+        }
+        private static string printarData(DateTime data)
+        {
+            return data.Day + "/" + data.Month + "/" + data.Year;
+        }
+        public int diasAberto(DateTime data)
+        {
+            TimeSpan diferenca = DateTime.Now.Subtract(data);
+            return Convert.ToInt32(diferenca.TotalDays);
+        }
+        public override String ToString()
+        {
+            return "Chamado{" + "Título:" + titulo + " " + equipamento.Nome + ", Data_abertura:" + printarData(data_abertura) + " Dias_em_aberto:" + diasAberto(data_abertura) + '}';
+        }
+    }
+}
---
+++ Equipamentos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Equipamentos_Junior
+{
+    class Equipamentos
+    {
+        private string nome;
+        private double preco;
+        private int nro_serie;
+        private DateTime data_fabricacao;
+        private string fabricante;
+
+        public Equipamentos(string nome, double preco, int nro_serie, DateTime data_fabricacao, string fabricante)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.nro_serie = nro_serie;
+            this.data_fabricacao = data_fabricacao;
+            this.fabricante = fabricante;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+
+        public override string ToString()
+        {
+            return "Equipamento{" + "Nome:" + nome + "/ Nro_serie:" + nro_serie + "/ Fabricante:" + fabricante + '}';
+        }
+    }
+}
